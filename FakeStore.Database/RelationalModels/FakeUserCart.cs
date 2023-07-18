79bd711dbbf1edce2b1c451198c28cc74c9c0586
alfredoaguiararce <alfredoaguiararce@gmail.com>
2023-07-18 00:23:20 -0700

    Create FakeUserCart.cs

---
+++ FakeStore.Database/RelationalModels/FakeUserCart.cs
@@ -0,0 +1,10 @@
+﻿using FakeStore.Database.Models;
+
+namespace FakeStore.Database.RelationalModels
+{
+    internal class FakeUserCart
+    {
+        public FakeUser User { get; set; }
+        public List<FakeProduct>? CartProducts { get; set; }
+    }
+}
